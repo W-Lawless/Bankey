2c070e214908a1c46274d062773cf8ea2154ed4e
W Lawless <wLawless.sharpe@protonmail.com>
2022-09-27 22:39:20 -0400

    table cell view

---
+++ Bankey/Bankey/AccountSummary/AccountSummaryCell.swift
@@ -0,0 +1,44 @@
+//
+//  AccountSummaryCell.swift
+//  Bankey
+//
+//  Created by W Lawless on 9/27/22.
+//
+
+import UIKit
+
+class AccountSummaryCell: UITableViewCell {
+    
+    let typeLabel = UILabel()
+    static let reuseID = "AccountSummaryCell"
+    static let rowHeight: CGFloat = 100
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setup()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension AccountSummaryCell {
+    private func setup() {
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+        typeLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        typeLabel.adjustsFontForContentSizeCategory = true
+        typeLabel.text = "Account Type"
+        
+        contentView.addSubview(typeLabel) // contentView, important
+        
+    }
+    
+    private func layout() {
+        NSLayoutConstraint.activate([
+            typeLabel.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
+            typeLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2)
+        ])
+    }
+}
